38b3b6da70c95b9111d122780d6e900fff197ece
7sayn7 <Nikolai.Kasatkin@urfu.me>
2023-05-19 17:25:23 +0500

    init project

---
+++ src/CalculatorTestDrive.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CalculatorTestDrive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+//        run(s);
+        //6 + 8 - > ["6", "+", "8"] - - - a operator b
+        // "+" -> toCharArray -> ['+']
+    }
+}
