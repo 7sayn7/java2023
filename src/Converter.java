6f8f6fb9cabe51989d26d8b955d0fe030e3eb5f9
7sayn7 <Nikolai.Kasatkin@urfu.me>
2023-05-27 17:43:17 +0500

    add arabicToRoman

--- src/Converter.java
+++ src/Converter.java
@@ -19,6 +19,54 @@ public class Converter {
         };
     }
 
+    public static String arabicToRoman(int arabic) {
+
+        /*  7 / 2 = 2
+            7 % 2 = 3
+            1021231232 % 100 = 2
+            123123123 / 100 = 1231231
+            ( 123123123 / 100 ) % 10 = 3
+            100
+        * */
+        int h = arabic / 100;  // 100 - > 1 - сотен
+        int t = (arabic / 10) % 10; // 100 -> 10 - > 0 - десятков
+        int o = arabic % 10; // 100 -> 0
+        String hundredsRoman = ""; // если не инициализируем, то просто создается ссылка которая никуда не указывает
+        String tensRoman = "";
+        String onesRoman = "";
+
+        switch (h) {
+            case 0 -> hundredsRoman = "";
+            case 1 -> hundredsRoman = "C";
+//            default -> hundredsRoman = "";
+        }
+        switch (t) {
+            case 0 -> tensRoman = "";
+            case 1 -> tensRoman = "X";
+            case 2 -> tensRoman = "XX";
+            case 3 -> tensRoman = "XXX";
+            case 4 -> tensRoman = "XL";
+            case 5 -> tensRoman = "L";
+            case 6 -> tensRoman = "LX";
+            case 7 -> tensRoman = "LXX";
+            case 8 -> tensRoman = "LXXX";
+            case 9 -> tensRoman = "XC";
+        }
+        switch (o) {
+            case 0 -> onesRoman = "";
+            case 1 -> onesRoman = "I";
+            case 2 -> onesRoman = "II";
+            case 3 -> onesRoman = "III";
+            case 4 -> onesRoman = "IV";
+            case 5 -> onesRoman = "V";
+            case 6 -> onesRoman = "VI";
+            case 7 -> onesRoman = "VII";
+            case 8 -> onesRoman = "VIII";
+            case 9 -> onesRoman = "IX";
+        }
+        return hundredsRoman + tensRoman + onesRoman;
+    }
+
     public static boolean isRomanNumber(String number) {
         return number.matches("I|II|III|IV|V|VI|VII|VIII|IX|X");
     }
